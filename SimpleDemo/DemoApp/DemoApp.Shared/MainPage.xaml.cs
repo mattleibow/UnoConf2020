5d567c78189a1b45fd909e7e7124e99e1d0f6c63
Matthew Leibowitz <mattleibow@live.com>
2020-08-03 14:39:10 +0200

    Add the simple, empty sample

---
+++ SimpleDemo/DemoApp/DemoApp.Shared/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.UWP;
+using Windows.UI.Xaml.Controls;
+
+namespace DemoApp
+{
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+		{
+			var canvas = e.Surface.Canvas;
+			var info = e.Info;
+
+			canvas.Clear();
+		}
+	}
+}
---
+++ SimpleDemo/DemoApp/DemoApp.Wasm/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoApp.Wasm
+{
+	public class Program
+	{
+		private static App _app;
+
+		public static int Main(string[] args)
+		{
+			Windows.UI.Xaml.Application.Start(_ => _app = new App());
+			return 0;
+		}
+	}
+}
